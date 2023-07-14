030ed375b36b0133104d2d8f10efe3badc3d3b16
Anwar <anwarhussaini160@gmail.com>
2023-07-14 04:47:20 +0430

    Add CSS files

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import './css/bootstrap.min.css';
+import './style.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
