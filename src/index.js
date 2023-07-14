6a744670ad183cc036fbe49142731051854a99bb
Anwar <anwarhussaini160@gmail.com>
2023-07-14 04:59:03 +0430

    Install and setup the React-Router-DOM package

--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import './css/bootstrap.min.css';
 import './style.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
+  <BrowserRouter>
     <App />
-  </React.StrictMode>
+  </BrowserRouter>
 );
