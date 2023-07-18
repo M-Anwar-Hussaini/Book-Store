5cdb62312ab516ca2702e9c8266ecf35c3127307
Anwar <anwarhussaini160@gmail.com>
2023-07-18 21:46:43 +0430

    Apply remove functionality for booklist

--- src/components/BookList.jsx
+++ src/components/BookList.jsx
@@ -1,8 +1,11 @@
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import Book from './Book';
+import { removeBook } from '../redux/books/bookSlice';
 
 function BookList() {
   const books = useSelector((store) => store.books);
+  const dispatch = useDispatch();
+
   return (
     <div className="p-4 border border-success rounded mt-3">
       <h1 className="fw-bold">Book List</h1>
@@ -12,6 +15,7 @@ function BookList() {
           category={el.category}
           title={el.title}
           key={el.item_id}
+          onDelete={() => dispatch(removeBook(el.item_id))}
         />
       ))}
     </div>
