f0c7a9230ff806c6e34a5daacf3a33063cd421e6
Anwar <anwarhussaini160@gmail.com>
2023-07-18 16:05:54 +0430

    Create categoriesSlice for category state management

---
+++ src/redux/categories/categoriesSlice.js
@@ -0,0 +1,16 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+const initialState = [];
+const categoriesSlice = createSlice({
+  name: 'categories',
+  initialState,
+  reducers: {
+    checkStatus(state, action) {
+      if (state.length) return;
+      state.push('Under construction');
+    },
+  },
+});
+
+export default categoriesSlice.reducer;
+export const { checkStatus } = categoriesSlice.actions;
