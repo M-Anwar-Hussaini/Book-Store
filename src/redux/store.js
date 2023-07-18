6c9b81274de984b7ca533c1c8e3b74191967a26a
Anwar <anwarhussaini160@gmail.com>
2023-07-18 16:21:20 +0430

    Create global store for the managing states in the src/redux/store.js

---
+++ src/redux/store.js
@@ -0,0 +1,13 @@
+import { configureStore } from '@reduxjs/toolkit';
+
+import bookSlice from './books/bookSlice';
+import categoriesSlice from './categories/categoriesSlice';
+
+const store = configureStore({
+  reducer: {
+    books: bookSlice,
+    categories: categoriesSlice,
+  },
+});
+
+export default store;
